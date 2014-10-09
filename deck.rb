c2f1ec5b0f985ca6980c238842527926baec1cbc
Andy Meneely <andy@se.rit.edu>
2014-10-09 18:39:16 -0400

    Rearranging bonuses and more game development

--- deck.rb
+++ deck.rb
@@ -1,22 +1,29 @@
 require 'squib'
 require_relative 'squib_helpers'
 
-color_dark = '#1A1917'
-color_bg = '#EDEBD8'
-
-deck = Squib.xlsx file: 'data/deck.xlsx'
+raw_deck = Squib.xlsx file: 'data/deck.xlsx'
+deck = explode_quantities(raw_deck)
 
 Squib::Deck.new(cards: deck['Title'].size, layout: 'layout.yml') do
-  background color: color_bg
+  background color: :background
 
   text str: deck['Title'], layout: :title
+  svg file: 'vp.svg', layout: :VP_img
 
-  # text str: 'Requires', layout: :Requires
-  # %w(Bonus1 Bonus2 Requires1 Requires2 Requires Description Snark).each do |key|
-  # 	text str: deck[key], layout: key
-  # end
+  range = [] ; deck['Requires1'].each_with_index { |n, i| range << i unless n.nil? }
+  text str: 'Requires:', layout: :Requires, range: range
 
-  png file: 'tgc-proof-overlay.png'
+  %w(Bonus1 Bonus2).each do |bonus|
+      range = [] # only put rectangles out in with non-nil texts
+      deck[bonus].each_with_index { |n, i| range << i unless n.nil? }
+      rect range: range, layout: "#{bonus}Box"
+  end 
+
+  %w(Bonus1 Bonus2 Requires Requires1 Requires2 Requires3 Description Snark VP).each do |key|
+    text str: deck[key], layout: key
+  end
+
+  #png file: 'tgc-proof-overlay.png'
 
   save format: :png
   save_json cards: @cards.size, deck: deck, file: "data/deck.json"
--- squib_helpers.rb
+++ squib_helpers.rb
@@ -13,3 +13,16 @@ def save_json(cards: 1, deck: {}, file: 'deck.json')
     f.write(JSON.pretty_generate(h))
   end
 end
+
+# Explode quantity field
+def explode_quantities(raw_deck)
+  qtys = raw_deck['Qty']
+  deck = {}
+  raw_deck.each do |col, arr|
+    deck[col] = []
+    qtys.each_with_index do |qty, index|
+      qty.to_i.times{ deck[col] << arr[index] }
+    end
+  end
+  return deck
+end
