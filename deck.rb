aeba9be76b40139bfebad8043df9e1be072158ca
Andy Meneely <andy@se.rit.edu>
2014-11-06 21:31:28 -0500

    Some game revisions, cut lines for pdf

--- deck.rb
+++ deck.rb
@@ -20,18 +20,19 @@ Squib::Deck.new(cards: deck['Title'].size, layout: 'layout.yml') do
       widths = bonusbox_width(deck[bonus], @layout[bonus]['font_size'])
       # puts widths
       rect range: range, layout: "#{bonus}Box", width: widths
-  end 
-
-
+  end
 
   %w(Bonus1 Bonus2 Requires Requires1 Requires2 Requires3 Description Snark VP).each do |key|
     text str: deck[key], layout: key
   end
 
   # png file: 'tgc-proof-overlay.png'
-
   save format: :png
-  # save format: :pdf, file: 'deck.pdf', trim: 38
+
+  #cut lines
+  rect x: 37, y: 37, width: 750, height: 1050, radius: 0, stroke_width: 3.0, stroke_color: 'black'
+  save format: :pdf, file: 'deck.pdf', trim: 37
+
   save_json cards: @cards.size, deck: deck, file: "data/deck.json"
   puts "Done. #{deck['Title'].size} cards"
 end
