6b7a7eb2aa8397398714136c084df8079c3848c2
Andy Meneely <andy@se.rit.edu>
2014-11-25 02:25:45 -0500

    Added backs deck

---
+++ backs.rb
@@ -0,0 +1,16 @@
+require 'squib'
+require_relative 'squib_helpers'
+
+deck = Squib.xlsx file: 'data/deck.xlsx'
+deck = explode_quantities(deck)
+
+Squib::Deck.new(cards: deck['Title'].size, width: 1125, height: 825,
+                layout: %w(layout.yml backs.yml)) do
+  background color: :background
+
+  text str: deck['Type'], layout: :type
+
+  save_png prefix: 'back_'
+  rect layout: :cut_line
+  save format: :pdf, file: 'backs.pdf', trim: 37
+end
