c695738c78255cedf1402f48594fb831d5ec7104
Andy Meneely <andy@se.rit.edu>
2015-01-20 17:11:31 -0500

    Playing with FontAwesome

--- deck.rb
+++ deck.rb
@@ -1,6 +1,9 @@
 require 'squib'
 require_relative 'squib_helpers'
 
+trash_icon = "<span font=\"FontAwesome\">\uF1F8</span> "
+requires_icon = "<span font=\"FontAwesome\">\uF08B</span> "
+
 deck = Squib.xlsx file: 'data/deck.xlsx'
 deck = explode_quantities(deck)
 
@@ -12,11 +15,19 @@ Squib::Deck.new(cards: deck['Title'].size, layout: 'layout.yml') do
   svg file: 'vp.svg', layout: :VP_img
 
   # Put the text "Requires" when necessary
-  range = [] ; deck['Requires1'].each_with_index { |n, i| range << i unless n.nil? }
-  text str: 'Requires:', layout: :Requires, range: range
+  # range = [] ; deck['Requires1'].each_with_index { |n, i| range << i unless n.nil? }
+  # text str: 'Requires:', layout: :Requires, range: range
   # Put the text "Trash" when necessary
-  range = [] ; deck['Trash1'].each_with_index { |n, i| range << i unless n.nil? }
-  text str: 'Trash:', layout: :Trash, range: range
+  # range = [] ; deck['Trash1'].each_with_index { |n, i| range << i unless n.nil? }
+  # text str: trash_icon, layout: :Trash, range: range, markup: true
+
+  %w(Trash1 Trash2).each do |bonus|
+    deck[bonus].map! { |str| str && (trash_icon + str)}
+  end
+
+  %w(Requires1 Requires2).each do |bonus|
+    deck[bonus].map! { |str| str && (requires_icon + str)}
+  end
 
   %w(Bonus1 Bonus2).each do |bonus|
       range = [] # only put rectangles out in with non-nil texts
@@ -26,13 +37,13 @@ Squib::Deck.new(cards: deck['Title'].size, layout: 'layout.yml') do
   end
 
   %w(Bonus1 Bonus2 Trash1 Trash2 Requires1 Requires2 Description Snark VP).each do |key|
-    text str: deck[key], layout: key
+    text str: deck[key], layout: key, markup: true
   end
 
   # png file: 'tgc-proof-overlay.png'
-  save format: :png
-  rect layout: :cut_line
-  save format: :pdf, file: 'deck.pdf', trim: 37
+  save format: :png#, range: 75
+  # rect layout: :cut_line
+  # save format: :pdf, file: 'deck.pdf', trim: 37
 
   save_json cards: @cards.size, deck: deck, file: "data/deck.json"
   puts "Done. #{deck['Title'].size} cards"
