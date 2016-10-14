efaeca05686335431c30ce76e38500b79f1ab9ce
Andy Meneely <andy@se.rit.edu>
2016-10-13 23:49:38 -0400

    Goofing around with a new look

--- deck.rb
+++ deck.rb
@@ -10,10 +10,11 @@ id = data['Title'].each.with_index.inject({}) { | hsh, (name, i)| hsh[name] = i;
 
 Squib::Deck.new(cards: data['Title'].size, layout: 'layout.yml') do
   background color: :background
+  svg file: 'steno pad.svg'
 
   text str: data['Title'], layout: :title
   text str: data['Type'], layout: :type
-  svg file: 'vp.svg', layout: :VP_img
+  svg file: 'vp-drawn.svg', layout: :VP_img
 
   %w(Trash1 Trash2).each do |bonus|
     data[bonus].map! { |str| str && (trash_icon + str)}
@@ -40,18 +41,24 @@ Squib::Deck.new(cards: data['Title'].size, layout: 'layout.yml') do
     text str: data[key], layout: key, markup: true
   end
 
-  rect layout: 'Description',
-       range: data['Description'].each.with_index.inject([]) {|rng, (d, i)| rng << i unless d.to_s.strip.empty?; rng}
+  # rect layout: 'Description',
+  #      range: data['Description'].each.with_index.inject([]) {|rng, (d, i)| rng << i unless d.to_s.strip.empty?; rng}
 
   # png file: 'tgc-proof-overlay.png'
   # save_png range: id['The Building Building Building']
 
-  save_png #all
+  # save_png #all
   # save_sheet prefix: 'sheet_', columns: 8, margin: 75, gap: 5, trim: 37
   # save format: :pdf, file: 'data.pdf', trim: 37
   # showcase file: 'showcase.png', range: [3,15,20, 90], fill_color: :black
+  save_png range: [1,72,46]
+  showcase range: [1,72], fill_color: :black
   rect layout: :cut_line
-  save_pdf trim: 37.5
+
+  # build(:pdf) do
+    save_pdf trim: 37.5
+  # end
+
 
   save_json cards: @cards.size, deck: data, file: "data/deck.json"
   puts "Done. #{data['Title'].size} cards"
