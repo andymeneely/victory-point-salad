8c8068530631eef6a1f62d2afe1b704985e00ad5
Andy Meneely <andy@se.rit.edu>
2020-04-25 02:26:52 -0400

    redoing the ladders

--- src/ladders.rb
+++ src/ladders.rb
@@ -1,18 +1,27 @@
 require 'squib'
 
-Squib::Deck.new(cards: 3, config: 'config.yml') do
+Squib::Deck.new(cards: 8, config: 'config.yml') do
   background color: :white
   use_layout file: 'layouts/specials.yml'
 
-  text layout: :title,
-       str: ['The Point Ladder', 'The Resource Ladder', 'The Special Ladder']
-  svg file: ['point ladder.svg', 'resource ladder.svg', 'special ladder.svg']
+  data = [
+    'The Point Ladder',
+    'The Resource Ladder',
+    'The Special Ladder',
+    'The Army Tracker',
+    'The Blue Veep Tracker',
+    'The Red Veep Tracker',
+    'The Green Veep Tracker',
+    'The Purple Veep Tracker'
+  ]
 
+  text layout: :title, str: data
+
+  svg file: data.map { |s| "#{s.downcase}.svg" }
+
+  # cut_zone
+  # safe_zone
   save_png prefix: 'ladder_'
-
-  rect layout: :cut_zone
-  rect layout: :safe_zone
-  save_png prefix: 'ladder_preview_', trim: 37.5, trim_radius: 37.5
 end
 
 Squib::Deck.new(cards: 3, config: 'config.yml') do
